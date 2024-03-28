3ade9d32dfafea0ca7fbadcd8eb7a5c3b655c518
AMAN <amam1326mi@gmail.com>
2024-03-28 17:26:04 +0530

    content added

---
+++ datatype.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0 
+// we will use mit license  as it is least restrictive license
+pragma solidity >=0.8.2 <0.9.0; //tell the code that this is the version i want you to use
+
+contract simplestorage {
+    // 4 basic type of datatype are
+    // bool,int,uint,address
+    bool a =true;
+    int b = -9;
+    uint c =9;
+    address d = 0xfe8b091535e5593dFd59bb3D300E0bc7Da397A94 ;
+    string e = "aman";
+    bytes a1 = "cat";
+    uint a2 ; // this will get initialized to 0
+
+}
